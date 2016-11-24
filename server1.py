a638727d5e1e2b3caac46d4640f4e6100485b6e9
amine-Qu <amhoume@gmail.com>
2016-11-24 15:30:25 -0500

    Add files via upload
    added some features

--- server1.py
+++ server1.py
@@ -1,16 +1,36 @@
 #!/usr/bin/python
 import socket
-import  sys
-host = ' '
-port = 1060
-s = socket.socket()
-s.bind(('',port)) #bind server
-s.listen(2)
-conn, addr = s.accept()
-print addr , "Now Connected"
-response =  conn.recv(1024)
-print response
-saved = open('saved_data.txt','w+')
-saved.write(response) # store the received information in txt file
+import sys
+import threading
 
-conn.close()
+
+def hanlder():
+    global s
+    host = ' '
+    port = 999
+    s = socket.socket()
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind(('', port))  # bind server
+    s.listen(5)
+    while True:
+        conn, addr = s.accept()
+        print addr, "Now Connected"
+        response = conn.recv(1024)
+        print  "recieved from " + str(addr[0])
+        saved = open('saved_data.txt', 'w+')
+        saved.write(response)  # store the received information in txt file
+        while True:
+            try:
+                def client_handler():
+                    teading = threading.Thread(target=hanlder())
+                    teading.start()
+                    print  response
+                client_handler()
+            except threading.ThreadError:
+                print " Error while handilung thread"
+                s.close()
+                sys.exit()
+        conn.close()
+
+
+hanlder()
